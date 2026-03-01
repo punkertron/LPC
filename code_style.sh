f839a5beecbba1d47df0834f0f1a6167fc282af8
Vladislav Shcherbatov <vladislav.a.shcherbatov@gmail.com>
2026-03-01 13:20:09 +0100

    Update code_style.sh to use clang-format and not clang-format-19

--- code_style.sh
+++ code_style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang-format-19 -i checkers-logic/*.{cpp,hpp} gui/*.{cpp,hpp} engine/*.{cpp,hpp} main.cpp
+clang-format -i checkers-logic/*.{cpp,hpp} gui/*.{cpp,hpp} engine/*.{cpp,hpp} main.cpp
